b77ed2e5661296ae45c7e7d23259d44d35772d52
dazidz <d.zimmermann22@gmail.com>
2026-07-07 15:18:12 +0200

    app.py verzeichniswechsel

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from supabase import create_client
+import os
+
+# Verbindung zu Supabase
+URL = "https://pyyyrbhxqpsngslazzpq.supabase.co/"
+KEY = "sb_publishable_aHdbyoX1tnJZStUvry2w5A_jrTeA1jC"
+supabase = create_client(url, key)
+
+st.title("Ticker-Screener Dashboard")
+
+# Daten laden
+response = supabase.table("DEINE_TABELLE").select("*").execute()
+st.dataframe(response.data)
