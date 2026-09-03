1bb8b09dd52a21bade23a36058ae1f31ddd61503
dazidz <d.zimmermann22@gmail.com>
2026-09-03 15:15:41 +0200

    debug

--- pages/Team.py
+++ pages/Team.py
@@ -1,3 +1,11 @@
+import sys
+from pathlib import Path
+
+# Setzt das Hauptverzeichnis fest in den Suchpfad von Python
+root_dir = Path(__file__).resolve().parent.parent
+if str(root_dir) not in sys.path:
+    sys.path.append(str(root_dir))
+
 import streamlit as st
 from supabase import create_client
 
